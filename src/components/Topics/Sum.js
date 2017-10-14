4245c0452fbc42f57613c881bcfccf8370b0d674
Bryce Hull <bahull@live.com>
2017-10-13 23:14:47 -0500

    Completed Sum

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,11 +1,46 @@
 import React, {Component} from "react"
 
 export default class Sum extends Component {
-    render() {
-      return (
-        <div>
-            <p>Sum</p>
-        </div>
-      )
+  constructor(props){
+    super(props)
+
+    this.state = {
+        number1: 0,
+        number2: 0,
+        sum: 0,
     }
+
+    
   }
+
+  handleState1 (val) {
+    this.setState({ number1: val});
+}
+handleState2 (val) {
+  this.setState({ number2: val});
+}
+   
+filterMethod(number1, number2){
+  var parseNum1 = parseInt(number1, 10);
+  var parseNum2 = parseInt(number2, 10);
+      var result = parseNum1 + parseNum2;
+      this.setState({sum: result})
+   
+  }
+
+
+
+
+
+render() {
+return (
+<div className="puzzleBox filterObjectPB">
+<h4>Sum</h4>
+<input className="inputLine" onChange={ e => this.handleState1(e.target.value) }></input>
+<input className="inputLine" onChange={ e => this.handleState2(e.target.value) }></input>
+<button className="confirmationButton" onClick={ () => this.filterMethod(this.state.number1, this.state.number2)}>Add</button>
+<span className="resultsBox filterObjectRB">Sum: { JSON.stringify(this.state.sum) }</span>
+</div>
+)
+}
+}
